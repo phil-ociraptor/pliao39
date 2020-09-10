8b0f4ee7092af82c7311cbf02a59d7f07b87bde8
Philip Liao <philipliao@gmail.com>
2020-09-10 14:49:18 -0500

    Add sitemap v1

---
+++ scripts/sitemap.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const fsPromises = fs.promises;
+const path = require("path");
+const { SitemapStream, streamToPromise } = require("sitemap");
+const { Readable } = require("stream");
+const { runInContext } = require("vm");
+
+const getAllFiles = function (dirPath, arrayOfFiles) {
+  files = fs.readdirSync(dirPath);
+
+  arrayOfFiles = arrayOfFiles || [];
+
+  files.forEach(function (file) {
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles);
+    } else {
+      arrayOfFiles.push(path.join(__dirname, dirPath, "/", file));
+    }
+  });
+
+  return arrayOfFiles;
+};
+
+const allPages = getAllFiles("./pages")
+  .map(f => f.split("pages")[1])
+  .map(f => f.split(".")[0])
+  .filter(f => f !== "index.js");
+
+const links = allPages.map(f => {
+  return {
+    url: f
+  };
+});
+
+const genSitemap = async links => {
+  const stream = new SitemapStream({ hostname: "https://pliao39.com" });
+  return streamToPromise(Readable.from(links).pipe(stream)).then(data =>
+    data.toString()
+  );
+};
+
+const run = async () => {
+  const xml = await genSitemap(links);
+  console.log(xml);
+  let sitemapFilePath = path.join(__dirname, "..", "public", "sitemap.xml");
+  fsPromises.writeFile(sitemapFilePath, xml);
+};
+
+run();
